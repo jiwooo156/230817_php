687290eac5322ecfa45c658b6f74f184c27040b3
gaebaljoa <wjdwldn156@gmail.com>
2023-10-25 18:21:37 +0900

    1025_hw

--- homework/1105/src/list.php
+++ homework/1105/src/list.php
@@ -1,12 +1,28 @@
 <?php
-define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/1105/src/");
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/1105/src/"); // 루트 상수 선언
+require_once(ROOT."lib/lib.php");
+
+$conn = null;
+// DB 연동
+try {
+   if(!my_db_conn($conn)) {
+    throw new Exception("DB Error : PDO Instance");
+    }; 
+    
+} catch(Exception $e) {
+    header("Location: /1105/src/error.php/?err_msg={$e->getMessage()}");
+    exit;
+} finally {
+    db_destroy_conn($conn); // DB 파기
+}
+
 
 
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
