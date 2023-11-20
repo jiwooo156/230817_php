b88bb6c9f5ba51f1a9702d13b49c963515522a56
gaebaljoa <wjdwldn156@naver.com>
2023-11-20 19:36:14 +0900

    1120_json 보강

--- test.js
+++ test.js
@@ -15,7 +15,60 @@ function getItem() {
 		document.body.appendChild(cp);
 	})
 	// 예외 일 경우
-	.catch();
+	.catch(error => console.log(error));
+}
 
-	return false;
+
+// 게시글 작성
+function addItem() {
+	fetch('http://localhost:8000/api/item', {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"data": {
+				"content": "안녕하세요 왜 안되지"
+			}
+		})
+	})
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.log(error))
+}
+
+
+// 게시글 수정
+function editItem() {
+	// 첫번째 파라미터: URL(세그먼트 파라미터란? 영향을 주고싶은 게시글id), 두번째 파라미터: 옵션
+	fetch('http://localhost:8000/api/item/3', {
+		method: 'PUT',
+		// headers: 통신할때 필요한 설정들 해줌.
+		headers: {
+			"Content-Type": "application/json"
+		},
+		// stringify() : 객체를 JSON으로 바꿔줌
+		body: JSON.stringify({
+			"data": {
+				"completed": "1"
+			}
+		})
+	})
+	// response로 받은 데이터 콘솔로 찍어봄
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.log(error))
+}
+
+
+// 게시글 삭제
+function destroyItem() {
+	// 첫번째 파라미터: URL(세그먼트 파라미터란? 영향을 주고싶은 게시글id), 두번째 파라미터: 옵션
+	fetch('http://localhost:8000/api/item/3', {
+		// 데이터를 안보내줘도 되기때문에 headers, body가 필요없음
+		method: 'DELETE'
+	})
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.log(error))
 }
